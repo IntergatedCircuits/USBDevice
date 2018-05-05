5fab6f3d4f6e5d59d5d638e5a436e3ed38f3b96c
Benedek Kupper <kupper.benedek@gmail.com>
2018-05-05 19:34:30 +0200

    HID: report item definitions structured

--- Class/HID/hid_report.h
+++ Class/HID/hid_report.h
@@ -25,71 +25,24 @@
 #define __HID_REPORT_H_
 #include <stdint.h>
 
-#define HID_USAGE_PAGE_VENDOR_SPEC      0x05, 0xFF /* Usage page: Vendor specific */
+/* Constructing HID report items */
 
-#define HID_USAGE_MIN_8(VAL)            0x19, (uint8_t)(VAL) /* Usage minimum 8 bit */
-#define HID_USAGE_MIN_16(VAL)           0x1A, _INTWO(VAL) /* Usage minimum 16 bit */
+typedef enum {
+    HID_ITEM_MAIN    = 0x00,
+    HID_ITEM_GLOBAL  = 0x04,
+    HID_ITEM_LOCAL   = 0x08,
+}HIDItemType;
 
-#define HID_USAGE_MAX_8(VAL)            0x29, (uint8_t)(VAL) /* Usage maximum 8 bit */
-#define HID_USAGE_MAX_16(VAL)           0x2A, _INTWO(VAL) /* Usage maximum 16 bit */
+#define HID_ITEM_0(TAG,TYPE)            (((TAG) << 4) | (HID_ITEM_##TYPE))
+#define HID_ITEM_1(TAG,TYPE,VAL)        (((TAG) << 4) | (HID_ITEM_##TYPE) | 0x1), (uint8_t)(VAL)
+#define HID_ITEM_2(TAG,TYPE,VAL)        (((TAG) << 4) | (HID_ITEM_##TYPE) | 0x2), _HID_INTWO(VAL)
+#define HID_ITEM_4(TAG,TYPE,VAL)        (((TAG) << 4) | (HID_ITEM_##TYPE) | 0x3), _HID_INFOUR(VAL)
+#define HID_ITEM_LONG(TAG,SIZE,...)     0xFE, (uint8_t)(SIZE), (uint8_t)(TAG), __VA_ARGS__
 
-#define HID_LOGICAL_MIN_8(VAL)          0x15, (uint8_t)(VAL) /* Logical minimum 8 bit */
-#define HID_LOGICAL_MIN_16(VAL)         0x16, _INTWO(VAL) /* Logical minimum 16 bit */
-#define HID_LOGICAL_MIN_32(VAL)         0x17, _INFOUR(VAL) /* Logical minimum 32 bit */
+#define _HID_INTWO(VAL)                 (uint8_t)((VAL) & 0xFF), (uint8_t)((VAL) >> 8)
+#define _HID_INFOUR(VAL)                _HID_INTWO(VAL), _HID_INTWO((VAL) >> 16)
 
-#define HID_LOGICAL_MAX_8(VAL)          0x25, (uint8_t)(VAL) /* Logical maximum 8 bit */
-#define HID_LOGICAL_MAX_16(VAL)         0x26, _INTWO(VAL) /* Logical maximum 16 bit */
-#define HID_LOGICAL_MAX_32(VAL)         0x27, _INFOUR(VAL) /* Logical maximum 32 bit */
-
-#define HID_REPORT_SIZE(VAL)            0x75, (uint8_t)(VAL) /* Report size 8 bit */
-
-#define HID_REPORT_COUNT(VAL)           0x95, (uint8_t)(VAL) /* Report count 8 bit */
-
-#define HID_REPORT_ID(VAL)              0x85, (uint8_t)(VAL) /* Report identifier 8 bit */
-
-#define HID_UNIT_EXPONENT(VAL)          0x55, (uint8_t)((VAL) & 0xF) /* Unit exponent 8 bit */
-
-#define HID_COLLECTION_PHYSICAL         0xA1, 0x00 /* Collection: Physical (group of axes) */
-#define HID_COLLECTION_APPLICATION      0xA1, 0x01 /* Collection: Application (mouse, keyboard) */
-#define HID_COLLECTION_LOGICAL          0xA1, 0x02 /* Collection: Logical (interrelated data) */
-#define HID_COLLECTION_REPORT           0xA1, 0x03 /* Collection: Report */
-#define HID_COLLECTION_NAMED_ARRAY      0xA1, 0x04 /* Collection: Named array */
-#define HID_COLLECTION_USAGE_SWITCH     0xA1, 0x05 /* Collection: Usage switch */
-#define HID_COLLECTION_USAGE_MOD        0xA1, 0x03 /* Collection: Usage modifier */
-#define HID_END_COLLECTION              0xc0       /* End Collection */
-
-/* Units not directly specified here can be handled by the software.
- * Examples:
- *   degC=DegK+273.15
- *   Kilogram=Gram with UnitExponent 3
- *   %=Units not specified
- */
-#define HID_UNIT_NONE                   0x65, 0x00
-#define HID_UNIT_LUX                    0x67, 0xE1, 0x00, 0x00, 0x01
-#define HID_UNIT_KELVIN                 0x67, 0x01, 0x00, 0x01, 0x00
-#define HID_UNIT_FAHRENHEIT             0x67, 0x03, 0x00, 0x01, 0x00
-#define HID_UNIT_PASCAL                 0x66, 0xF1, 0xE1
-#define HID_UNIT_NEWTON                 0x66, 0x11, 0xE1
-#define HID_UNIT_METERS_PER_SEC         0x66, 0x11, 0xF0
-#define HID_UNIT_METERS_PER_SEC2        0x66, 0x11, 0xE0
-#define HID_UNIT_FARAD                  0x67, 0xE1, 0x4F, 0x20, 0x00
-#define HID_UNIT_AMPERE                 0x67, 0x01, 0x00, 0x10, 0x00
-#define HID_UNIT_AMPERE_PER_SEC         0x67, 0x01, 0x10, 0x10, 0x00
-#define HID_UNIT_WATT                   0x66, 0x21, 0xD1
-#define HID_UNIT_HENRY                  0x67, 0x21, 0xE1, 0xE0, 0x00
-#define HID_UNIT_OHM                    0x67, 0x21, 0xD1, 0xE0, 0x00
-#define HID_UNIT_VOLT                   0x67, 0x21, 0xD1, 0xF0, 0x00
-#define HID_UNIT_HERTZ                  0x66, 0x01, 0xF0
-#define HID_UNIT_DEGREES                0x65, 0x14
-#define HID_UNIT_DEGREES_PER_SEC        0x66, 0x14, 0xF0
-#define HID_UNIT_DEGREES_PER_SEC2       0x66, 0x14, 0xE0
-#define HID_UNIT_RADIANS                0x65, 0x12
-#define HID_UNIT_RADIANS_PER_SEC        0x66, 0x12, 0xF0
-#define HID_UNIT_RADIANS_PER_SEC2       0x66, 0x12, 0xE0
-#define HID_UNIT_SECOND                 0x66, 0x01, 0x10
-#define HID_UNIT_GAUSS                  0x67, 0x01, 0xE1, 0xF0, 0x00
-#define HID_UNIT_GRAM                   0x66, 0x01, 0x01
-#define HID_UNIT_CENTIMETER             0x65, 0x11
+/* HID Report Main Items */
 
 typedef enum
 {
@@ -116,11 +69,111 @@ typedef enum
 #define HID_ITEM_FLAG_VOLATILE          0x80 /* Volatile? */
 #define HID_ITEM_FLAG_BUFF_BYTES        0x0100 /* Buffered bytes vs Bit-field */
 
-#define HID_INPUT(FLAGS)                0x81, (uint8_t)(FLAGS) /* Input with 1 byte */
-#define HID_OUTPUT(FLAGS)               0x92, _INTWO(FLAGS) /* Output with 2 bytes */
-#define HID_FEATURE(FLAGS)              0xB2, _INTWO(FLAGS) /* Feature with 2 bytes */
+#define HID_INPUT(FLAGS)                HID_ITEM_1(0x8, MAIN, FLAGS)
+#define HID_OUTPUT(FLAGS)               HID_ITEM_2(0x9, MAIN, FLAGS)
+#define HID_FEATURE(FLAGS)              HID_ITEM_2(0xB, MAIN, FLAGS)
 
-#define _INTWO(VAL)                     (uint8_t)((VAL) & 0xFF), (uint8_t)((VAL) >> 8)
-#define _INFOUR(VAL)                    _INTWO(VAL), _INTWO((VAL) >> 16)
+#define HID_COLLECTION_PHYSICAL         HID_ITEM_1(0xA, MAIN, 0x00) /* Collection: Physical (group of axes) */
+#define HID_COLLECTION_APPLICATION      HID_ITEM_1(0xA, MAIN, 0x01) /* Collection: Application (mouse, keyboard) */
+#define HID_COLLECTION_LOGICAL          HID_ITEM_1(0xA, MAIN, 0x02) /* Collection: Logical (interrelated data) */
+#define HID_COLLECTION_REPORT           HID_ITEM_1(0xA, MAIN, 0x03) /* Collection: Report */
+#define HID_COLLECTION_NAMED_ARRAY      HID_ITEM_1(0xA, MAIN, 0x04) /* Collection: Named array */
+#define HID_COLLECTION_USAGE_SWITCH     HID_ITEM_1(0xA, MAIN, 0x05) /* Collection: Usage switch */
+#define HID_COLLECTION_USAGE_MOD        HID_ITEM_1(0xA, MAIN, 0x06) /* Collection: Usage modifier */
+#define HID_END_COLLECTION              HID_ITEM_0(0xC, MAIN)
+
+/* HID Report Global Items */
+
+#define HID_USAGE_PAGE_VENDOR_SPEC      HID_ITEM_1(0x0, GLOBAL, 0xFF)
+
+#define HID_LOGICAL_MIN_8(VAL)          HID_ITEM_1(0x1, GLOBAL, VAL)
+#define HID_LOGICAL_MIN_16(VAL)         HID_ITEM_2(0x1, GLOBAL, VAL)
+#define HID_LOGICAL_MIN_32(VAL)         HID_ITEM_4(0x1, GLOBAL, VAL)
+
+#define HID_LOGICAL_MAX_8(VAL)          HID_ITEM_1(0x2, GLOBAL, VAL)
+#define HID_LOGICAL_MAX_16(VAL)         HID_ITEM_2(0x2, GLOBAL, VAL)
+#define HID_LOGICAL_MAX_32(VAL)         HID_ITEM_4(0x2, GLOBAL, VAL)
+
+#define HID_PHYSICAL_MIN_8(VAL)         HID_ITEM_1(0x3, GLOBAL, VAL)
+#define HID_PHYSICAL_MIN_16(VAL)        HID_ITEM_2(0x3, GLOBAL, VAL)
+#define HID_PHYSICAL_MIN_32(VAL)        HID_ITEM_4(0x3, GLOBAL, VAL)
+
+#define HID_PHYSICAL_MAX_8(VAL)         HID_ITEM_1(0x4, GLOBAL, VAL)
+#define HID_PHYSICAL_MAX_16(VAL)        HID_ITEM_2(0x4, GLOBAL, VAL)
+#define HID_PHYSICAL_MAX_32(VAL)        HID_ITEM_4(0x4, GLOBAL, VAL)
+
+/* valid range: -8 .. 7 */
+#define HID_UNIT_EXPONENT(VAL)          HID_ITEM_1(0x5, GLOBAL, (uint8_t)((VAL) & 0xF))
+
+/* Since [cm] and [g] are used instead of SI [m] and [kg],
+ * the latter and their derived units also have an exponent defined */
+#define HID_UNIT_NONE                   HID_ITEM_1(0x6, GLOBAL, 0x00000000)
+#define HID_UNIT_CENTIMETER             HID_ITEM_1(0x6, GLOBAL, 0x00000011)
+#define HID_UNIT_METER                  HID_UNIT_CENTIMETER
+#define HID_UNIT_METER_EXP              2
+#define HID_UNIT_RADIAN                 HID_ITEM_1(0x6, GLOBAL, 0x00000012)
+#define HID_UNIT_DEGREE                 HID_ITEM_1(0x6, GLOBAL, 0x00000014)
+#define HID_UNIT_GRAM                   HID_ITEM_2(0x6, GLOBAL, 0x00000101)
+#define HID_UNIT_KILOGRAM               HID_UNIT_GRAM
+#define HID_UNIT_KILOGRAM_EXP           3
+#define HID_UNIT_SECOND                 HID_ITEM_2(0x6, GLOBAL, 0x00001001)
+#define HID_UNIT_KELVIN                 HID_ITEM_4(0x6, GLOBAL, 0x00010001)
+#define HID_UNIT_FAHRENHEIT             HID_ITEM_4(0x6, GLOBAL, 0x00010003)
+#define HID_UNIT_AMPERE                 HID_ITEM_4(0x6, GLOBAL, 0x00100001)
+#define HID_UNIT_CANDELA                HID_ITEM_4(0x6, GLOBAL, 0x01000001)
+#define HID_UNIT_NEWTON                 HID_ITEM_2(0x6, GLOBAL, 0x0000E111)
+#define HID_UNIT_NEWTON_EXP             (HID_UNIT_METER_EXP + HID_UNIT_KILOGRAM_EXP)
+#define HID_UNIT_METER_PER_SEC          HID_ITEM_2(0x6, GLOBAL, 0x0000F011)
+#define HID_UNIT_METER_PER_SEC_EXP      (HID_UNIT_METER_EXP)
+#define HID_UNIT_METER_PER_SEC2         HID_ITEM_2(0x6, GLOBAL, 0x0000E011)
+#define HID_UNIT_METER_PER_SEC2_EXP     (HID_UNIT_METER_EXP)
+#define HID_UNIT_PASCAL                 HID_ITEM_2(0x6, GLOBAL, 0x0000E1F1)
+#define HID_UNIT_PASCAL_EXP             (HID_UNIT_KILOGRAM_EXP - HID_UNIT_METER_EXP)
+#define HID_UNIT_JOULE                  HID_ITEM_2(0x6, GLOBAL, 0x0000E121)
+#define HID_UNIT_JOULE_EXP              ((HID_UNIT_METER_EXP*2) + HID_UNIT_KILOGRAM_EXP)
+#define HID_UNIT_HERTZ                  HID_ITEM_2(0x6, GLOBAL, 0x0000F001)
+#define HID_UNIT_DEGREE_PER_SEC         HID_ITEM_2(0x6, GLOBAL, 0x0000F014)
+#define HID_UNIT_DEGREE_PER_SEC2        HID_ITEM_2(0x6, GLOBAL, 0x0000E014)
+#define HID_UNIT_RADIAN_PER_SEC         HID_ITEM_2(0x6, GLOBAL, 0x0000F012)
+#define HID_UNIT_RADIAN_PER_SEC2        HID_ITEM_2(0x6, GLOBAL, 0x0000E012)
+#define HID_UNIT_WATT                   HID_ITEM_2(0x6, GLOBAL, 0x0000D121)
+#define HID_UNIT_WATT_EXP               ((HID_UNIT_METER_EXP*2) + HID_UNIT_KILOGRAM_EXP)
+#define HID_UNIT_AMPERE_PER_SEC         HID_ITEM_4(0x6, GLOBAL, 0x0010F001)
+#define HID_UNIT_COULOMB                HID_ITEM_4(0x6, GLOBAL, 0x00101001)
+#define HID_UNIT_FARAD                  HID_ITEM_4(0x6, GLOBAL, 0x00204FE1)
+#define HID_UNIT_FARAD_EXP              ((HID_UNIT_METER_EXP*-2) - HID_UNIT_KILOGRAM_EXP)
+#define HID_UNIT_GAUSS                  HID_ITEM_4(0x6, GLOBAL, 0x00F0E101)
+#define HID_UNIT_GAUSS_EXP              (HID_UNIT_KILOGRAM_EXP)
+#define HID_UNIT_HENRY                  HID_ITEM_4(0x6, GLOBAL, 0x00E0E121)
+#define HID_UNIT_HENRY_EXP              ((HID_UNIT_METER_EXP*2) + HID_UNIT_KILOGRAM_EXP)
+#define HID_UNIT_LUX                    HID_ITEM_4(0x6, GLOBAL, 0x010000E1)
+#define HID_UNIT_LUX_EXP                (HID_UNIT_METER_EXP*-2)
+#define HID_UNIT_OHM                    HID_ITEM_4(0x6, GLOBAL, 0x00E0D121)
+#define HID_UNIT_OHM_EXP                ((HID_UNIT_METER_EXP*2) + HID_UNIT_KILOGRAM_EXP)
+#define HID_UNIT_SIEMENS                HID_ITEM_4(0x6, GLOBAL, 0x00203FE1)
+#define HID_UNIT_SIEMENS_EXP            ((HID_UNIT_METER_EXP*-2) - HID_UNIT_KILOGRAM_EXP)
+#define HID_UNIT_TESLA                  HID_ITEM_4(0x6, GLOBAL, 0x00F0E101)
+#define HID_UNIT_TESLA_EXP              (HID_UNIT_KILOGRAM_EXP)
+#define HID_UNIT_VOLT                   HID_ITEM_4(0x6, GLOBAL, 0x00F0D121)
+#define HID_UNIT_VOLT_EXP               ((HID_UNIT_METER_EXP*2) + HID_UNIT_KILOGRAM_EXP)
+#define HID_UNIT_WEBER                  HID_ITEM_4(0x6, GLOBAL, 0x00F0E121)
+#define HID_UNIT_WEBER_EXP              ((HID_UNIT_METER_EXP*2) + HID_UNIT_KILOGRAM_EXP)
+
+#define HID_REPORT_SIZE(VAL)            HID_ITEM_1(0x7, GLOBAL, VAL)
+#define HID_REPORT_ID(VAL)              HID_ITEM_1(0x8, GLOBAL, VAL)
+#define HID_REPORT_COUNT(VAL)           HID_ITEM_1(0x9, GLOBAL, VAL)
+
+#define HID_PUSH                        HID_ITEM_0(0xA, GLOBAL)
+#define HID_POP                         HID_ITEM_0(0xA, GLOBAL)
+
+/* HID Report Local Items */
+
+#define HID_USAGE_MIN_8(VAL)            HID_ITEM_1(0x1, LOCAL, VAL)
+#define HID_USAGE_MIN_16(VAL)           HID_ITEM_2(0x1, LOCAL, VAL)
+
+#define HID_USAGE_MAX_8(VAL)            HID_ITEM_1(0x2, LOCAL, VAL)
+#define HID_USAGE_MAX_16(VAL)           HID_ITEM_2(0x2, LOCAL, VAL)
+
+#define HID_STRING_INDEX(VAL)           HID_ITEM_1(0x7, LOCAL, VAL)
 
 #endif /* __HID_REPORT_H_ */
